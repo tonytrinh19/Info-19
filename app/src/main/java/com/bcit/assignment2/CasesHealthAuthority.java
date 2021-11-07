e5858a512131a36fa33e04bb4556add414f12059
Jordan Godau <jordangunn92@gmail.com>
2021-11-06 21:07:48 -0700

    Added health authority activity

--- app/src/main/java/com/bcit/assignment2/CasesHealthAuthority.java
+++ app/src/main/java/com/bcit/assignment2/CasesHealthAuthority.java
@@ -6,13 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class CasesHealthAuthority extends AppCompatActivity {
 
-    ArrayList<String> healthAuthorityCases;
-    ListView lv;
+    String Fraser;
+    String Interior;
+    String Northern;
+    String Canada;
+    String Vancouver;
+    String Coastal;
+
+    TextView fraserCases;
+    TextView interiorCases;
+    TextView northernCases;
+    TextView canadaCases;
+    TextView vancouverCases;
+    TextView coastalCases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +32,28 @@ public class CasesHealthAuthority extends AppCompatActivity {
         setContentView(R.layout.activity_cases_gender);
 
         Intent intent = getIntent();
-        healthAuthorityCases = intent.getStringArrayListExtra("db_result");
-        lv = findViewById(R.id.health_authority_list);
-        ArrayAdapter<String> ad = new ArrayAdapter<>(
-                this,
-                R.layout.support_simple_spinner_dropdown_item,
-                healthAuthorityCases
-        );
-        lv.setAdapter(ad);
+
+        Fraser = intent.getStringExtra("lessThan10");
+        Interior = intent.getStringExtra("to19");
+        Northern = intent.getStringExtra("to29");
+        Canada = intent.getStringExtra("to39");
+        Vancouver = intent.getStringExtra("to49");
+        Coastal = intent.getStringExtra("to59");
+
+        fraserCases = findViewById(R.id.to19);
+        interiorCases = findViewById(R.id.to29);
+        northernCases = findViewById(R.id.to39);
+        canadaCases = findViewById(R.id.to49);
+        vancouverCases = findViewById(R.id.to59);
+        coastalCases = findViewById(R.id.to69);
+
+        fraserCases.setText(getString(R.string   .Fraser,   fraserCases));
+        interiorCases.setText(getString(R.string .Interior, interiorCases));
+        northernCases.setText(getString(R.string .Northern, northernCases));
+        canadaCases.setText(getString(R.string   .Canada,   canadaCases));
+        vancouverCases.setText(getString(R.string.Vancouver,vancouverCases));
+        coastalCases.setText(getString(R.string      .Coastal,  coastalCases));
+
 
     }
 }
